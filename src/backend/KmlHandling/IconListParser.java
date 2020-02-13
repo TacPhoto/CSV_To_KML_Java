8ead85346ec55172a683ebd3ca050696a5b44c22
TacPhoto <karol.kowalczyk.99@gmail.com>
2020-02-13 23:26:40 +0100

    create IconListParser sceleton

---
+++ src/backend/KmlHandling/IconListParser.java
@@ -0,0 +1,21 @@
+package backend.KmlHandling;
+
+import java.io.IOException;
+import java.util.List;
+
+public class IconListParser{
+    List<String> iconList;
+
+    public IconListParser(String kmlPath){
+        List<String> iconList = null;
+
+        //todo: declare buffered reader for the kml
+    }
+
+    //todo: read kml file as text until you get to <Folder> phrase - save it into one long string
+
+    /*todo: read the newly created string. search for text between <Style id=" and the closest "
+    * then parse them into iconList
+    */
+    
+}
